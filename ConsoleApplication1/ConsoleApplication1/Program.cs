6ca2dca0c94a3ab2f915f984efa881f19a66c95d
Vadim Alexeev <valexeev@distillery.com>
2016-09-19 17:56:46 +0300

    build break

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,6 +13,8 @@ namespace ConsoleApplication1
             Console.WriteLine("Hello everybody!");
             Console.WriteLine("Move your body everybody!");
             Console.WriteLine("Move move!");
+            
+            breaking changes
         }
     }
 }
