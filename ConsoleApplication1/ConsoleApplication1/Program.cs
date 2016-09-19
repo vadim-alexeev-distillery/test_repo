c751ee3d0f8e8a574b347d798f91a69c9c38c6f9
vadim-alexeev-distillery <valexeev@distillery.com>
2016-09-19 17:41:20 +0300

    111

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello everybody!");
+            Console.WriteLine("Move your body everybody!");
         }
     }
 }
