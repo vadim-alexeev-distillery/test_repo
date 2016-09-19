8db57e9310dcff0a9d36614fafda6fb3486c9725
vadim-alexeev-distillery <valexeev@distillery.com>
2016-09-19 17:43:13 +0300

    one mode change

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("Hello everybody!");
             Console.WriteLine("Move your body everybody!");
+            Console.WriteLine("Move move!");
         }
     }
 }
